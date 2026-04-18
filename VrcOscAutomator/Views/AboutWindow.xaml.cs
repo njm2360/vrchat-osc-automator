d2b6ee78589d19c6e77e2343bc4da8960fef7b41
njm2360 <me@njm2360.com>
2026-04-19 00:13:25 +0900

    Add app icon

--- VrcOscAutomator/Views/AboutWindow.xaml.cs
+++ VrcOscAutomator/Views/AboutWindow.xaml.cs
@@ -1,6 +1,7 @@
 using System.Diagnostics;
 using System.Reflection;
 using System.Windows;
+using System.Windows.Media.Imaging;
 
 namespace VrcOscAutomator.Views;
 
@@ -9,6 +10,12 @@ public partial class AboutWindow : Window
     public AboutWindow()
     {
         InitializeComponent();
+
+        var decoder = new IconBitmapDecoder(
+            new Uri("pack://application:,,,/Resources/icon.ico"),
+            BitmapCreateOptions.None, BitmapCacheOption.Default);
+        AppIcon.Source = decoder.Frames.OrderBy(f => Math.Abs(f.PixelWidth - 128)).First();
+
         var version = Assembly.GetExecutingAssembly().GetName().Version;
         VersionText.Text = version is not null ? $"バージョン {version.Major}.{version.Minor}.{version.Build}" : "バージョン不明";
     }
